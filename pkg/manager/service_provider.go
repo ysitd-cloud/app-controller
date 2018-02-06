099a0687e9ab9597dfa79067d406ee538c9da353
Tony Yip <tony@opensource.hk>
2018-02-06 13:57:08 +0800

    Export more in service worker

--- pkg/manager/service_provider.go
+++ pkg/manager/service_provider.go
@@ -21,14 +21,53 @@ func CreateManagerServiceProvider(app container.Container) container.ServiceProv
 func (*managerServiceProvider) Provides() []string {
 	return []string{
 		"manager",
+		"manager.app",
+		"manager.deployment",
+		"manager.env",
+		"manager.network",
 	}
 }
 
 func (*managerServiceProvider) Register(app container.Container) {
-	app.Bind("manager", func(app container.Container) interface{} {
-		m := new(manager)
+	app.Bind("manager.app", func(app container.Container) interface{} {
 		db := app.Make("db.pool").(db.Pool)
-		m.SetDB(db)
+		store := new(appStore)
+		store.db = db
+		return store
+	})
+
+	app.Bind("manager.deployment", func(app container.Container) interface{} {
+		db := app.Make("db.pool").(db.Pool)
+		store := new(deploymentStore)
+		store.db = db
+		return store
+	})
+
+	app.Bind("manager.env", func(app container.Container) interface{} {
+		db := app.Make("db.pool").(db.Pool)
+		store := new(envStore)
+		store.db = db
+		return store
+	})
+
+	app.Bind("manager.network", func(app container.Container) interface{} {
+		db := app.Make("db.pool").(db.Pool)
+		store := new(networkStore)
+		store.db = db
+		return store
+	})
+
+	app.Bind("manager", func(app container.Container) interface{} {
+		as := app.Make("manager.app").(*appStore)
+		ds := app.Make("manager.deployment").(*deploymentStore)
+		es := app.Make("manager.env").(*envStore)
+		ns := app.Make("manager.network").(*networkStore)
+		m := &manager{
+			app:        as,
+			deployment: ds,
+			env:        es,
+			network:    ns,
+		}
 		return m
 	})
 }
